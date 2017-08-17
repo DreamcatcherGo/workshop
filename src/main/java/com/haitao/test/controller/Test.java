777c16ed846bf3d747d0a7ed0c14ca6bea160a42
dreamcatchergo <dreamcatcherGo@sina.com>
2017-08-17 22:31:14 +0800

    的点点滴滴

--- src/main/java/com/haitao/test/controller/Test.java
+++ src/main/java/com/haitao/test/controller/Test.java
@@ -14,6 +14,7 @@ public class Test {
             if (!file.exists()) {
                 file.createNewFile();
             }
+
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write("2");
