32af249e6d870e0e4957fa755579ed30779b8409
dreamcatchergo <dreamcatcherGo@sina.com>
2017-10-17 18:14:00 +0800

    111111

--- src/main/java/com/haitao/test/controller/UploadController.java
+++ src/main/java/com/haitao/test/controller/UploadController.java
@@ -163,6 +163,7 @@ public class UploadController {
             File file = new File(fileName);
             if (!file.exists()) {
                 file.createNewFile();
+
             }
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
